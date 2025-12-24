f9a8eab6f887abc90d983a695df94b8a85481b65
jsneha9321 <149572127+jsneha9321@users.noreply.github.com>
2025-12-24 13:39:05 +0530

    Initial portfolio upload

---
+++ Solution1/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
