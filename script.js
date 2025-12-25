7b4f814ea96736ae2b9205bfdca87218792dd4c2
jsneha9321 <149572127+jsneha9321@users.noreply.github.com>
2025-12-25 13:26:42 +0530

    script.js

--- script.js
+++ script.js
@@ -5,10 +5,3 @@ window.addEventListener("scroll", () => {
     }
   });
 });
-window.addEventListener("scroll", () => {
-  document.querySelectorAll(".reveal").forEach(el => {
-    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-});
